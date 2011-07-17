f1eeed23f2c811927662823e2db5654bcd3afb52
Matthew Orres <matthew.orres@gmail.com>
2011-07-17 15:32:45 -0400

    Add the manager presenter

---
+++ manager.php
@@ -0,0 +1,18 @@
+<?php
+require_once("check_login.php");
+require_once("models/manager_object.php");
+
+DEFINE("ACTIVE_TAB", "CONTROL_PANEL");
+
+DEFINE('DRAFT_ID', intval($_GET['mid']));
+
+switch($_GET['action']) {
+	case 'moveManagerUp':
+		
+		break;
+	
+	case 'moveManagerDown':
+		
+		break;
+}
+?>
