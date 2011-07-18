f4c23afc548d1e161367cbaec5e491c36084a372
Matthew Orres <matthew.orres@gmail.com>
2011-07-17 21:19:53 -0400

    Start stubbing out manager presenter to lay out path for next development.

--- manager.php
+++ manager.php
@@ -8,6 +8,7 @@ DEFINE('MANAGER_ID', intval($_GET['mid']));
 
 switch($_GET['action']) {
 	case 'moveManager':
+		// <editor-fold defaultstate="collapsed" desc="moveManager Logic">
 		$manager_id = $_POST['mid'];
 		$direction = $_POST['direction'];
 		
@@ -22,6 +23,27 @@ switch($_GET['action']) {
 		}	
 		
 		echo $success ? "SUCCESS" : "FAILURE";
+		// </editor-fold>
+		break;
+	
+	case 'editManager':
+		//TODO: Implement the view/error screen here
+		break;
+	
+	case 'updateManager':
+		//TODO: Implement the manager save here
+		break;
+	
+	case 'deleteManager':
+		//TODO: Implement the confirm action screen here
+		break;
+	
+	case 'removeManager':
+		//TODO: Implement the manager delete here
+		break;
+	
+	case 'addManagers':
+		//TODO: Implement the bulk add-managers screen here
 		break;
 }
 ?>
