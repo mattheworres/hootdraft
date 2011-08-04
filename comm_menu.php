6504a1a50c377a17a78265dbaa1776c2ab340a07
Matthew Orres <matthew.orres@gmail.com>
2011-08-03 21:00:39 -0400

    YES! PURGE OF THE EXCESS! WHOO!

--- comm_menu.php
+++
@@ -1,8 +0,0 @@
-<?php require_once("login_fcns.php"); ?><div id="right_side">
-	<h3>Draft Links</h3>
-	 <?php if(CONTROL_PANEL_ACTION != "CREATE") {?><p><a href="control_panel.php?action=createDraft"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-plusthick"></span>Create Draft</a></p><?php } ?>
-	 <p><a href="control_panel.php"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-star"></span>Control Panel Home</a></p>
-	<div class='featurebox_side'>In order to add managers or start a draft, you must create or select a draft first.</div>
-	<?php if(isLoggedIn()) {?><div class="featurebox_side">You are logged in as <strong><?php echo $_SESSION['username']; ?></strong></div>
-	<?php }?>
-</div>
