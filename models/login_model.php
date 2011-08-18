225351e339d69dd10330c9b5084fe4cc279821a5
Matthew Orres <matthew.orres@gmail.com>
2011-08-17 23:13:55 -0400

    Finish login model PDO integration.

--- models/login_model.php
+++ models/login_model.php
@@ -2,7 +2,7 @@
 
 class loginObject {
 	public static function get_login_status() {
-		global $LOGGED_IN_USER;
+		global $LOGGED_IN_USER; /* @var $LOGGED_IN_USER user_object */
 
 		if($LOGGED_IN_USER->userAuthenticated() || ($LOGGED_IN_USER->user_id > 0 && strlen($LOGGED_IN_USER->user_name) > 0 && strlen($LOGGED_IN_USER->password) > 0))
 		{
@@ -27,16 +27,17 @@ class loginObject {
 	}
 
 	public static function authenticate_user($raw_username, $raw_password) {
-		global $LOGGED_IN_USER;
+		global $LOGGED_IN_USER; /* @var $LOGGED_IN_USER user_object */
+		global $DBH; /* @var $DBH PDO */
+		
 		$LOGGED_IN_USER->user_name = $raw_username;
 		$LOGGED_IN_USER->password = user_object::getHashedPassword($raw_password);
-
-		$user_result = mysql_query("SELECT UserID, Username, Password
-									FROM user_login
-									WHERE Username = '" . mysql_real_escape_string($LOGGED_IN_USER->user_name) . "' AND
-									Password = '" . mysql_real_escape_string($LOGGED_IN_USER->password) . "'
-							   ");
-		if(!$user_row = mysql_fetch_array($user_result)) {
+		
+		$user_stmt = $DBH->prepare("SELECT UserID, Username, Password FROM user_login WHERE Username = ? AND Password = ?");
+		$user_stmt->bindParam(1, $LOGGED_IN_USER->user_name);
+		$user_stmt->bindParam(2, $LOGGED_IN_USER->password);
+		
+		if(!$user_stmt->execute()) {
 			return false;
 		}else{
 			$LOGGED_IN_USER->user_id = $user_row['UserID'];
