e1bf91fb0f7b420694cfd9dbcfa467accebff0e3
Matthew Orres <matthew.orres@gmail.com>
2011-08-20 18:56:40 -0400

    Grr. Another bad link.

--- views/shared/draft_room_menu.php
+++ views/shared/draft_room_menu.php
@@ -5,6 +5,6 @@ if(DRAFT_ID > 0) {
 	<h4><a href="draft_room.php?did=<?php echo DRAFT_ID; ?>"><strong><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-circle-check"></span>Main Draft Room</strong></a></h4>
 	<p><a href="draft_room.php?action=addScreen&did=<?php echo DRAFT_ID; ?>"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-plusthick"></span>Make a Pick</a></p>
 	<p><a href="draft_room.php?action=selectPickToEdit&did=<?php echo DRAFT_ID; ?>"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-pencil"></span>Edit a Pick</a></p>
-	<p><a href="public_draft.php?action=draftBoard&draft_id=<?php echo DRAFT_ID; ?>"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-calculator"></span>View Public Board</a></p>
+	<p><a href="public_draft.php?action=draftBoard&did=<?php echo DRAFT_ID; ?>"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-calculator"></span>View Public Board</a></p>
 	<p><a href="draft.php?did=<?php echo DRAFT_ID; ?>"><span style="display: inline-block; vertical-align: middle; margin-right: 2px;" class="ui-icon ui-icon-arrowreturnthick-1-w"></span>Back to Manage Page</a></p>
 </div><?php } ?>
