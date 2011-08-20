7a2843252c0ff0b8f05eb8b0b958a8d26397fb02
Matthew Orres <matthew.orres@gmail.com>
2011-08-20 19:08:46 -0400

    Fix for not closing draft after last pick has been made.

--- draft_room.php
+++ draft_room.php
@@ -27,7 +27,9 @@ if($DRAFT->isUndrafted()) {
 	define("PAGE_CONTENT", "Your draft is currently set to \"Setting Up \", which means that the draft room isn't officially open yet. Once you've set your draft to \"In Progress\", come back here and you can begin entering picks for your draft.");
 	require_once("views/shared/generic_result_view.php");
 	exit(0);
-}elseif($DRAFT->isCompleted()) {
+}
+
+if($DRAFT->isCompleted()) {
 	define("PAGE_HEADER", "Draft Room is Closed - Your Draft is Over!");
 	define("P_CLASS", "success");
 	define("PAGE_CONTENT", "The draft room is officially closed.  Your draft is officially complete, all picks have been made and now all you have to do is get the results entered into your league.<br /><br /><a href=\"public_draft.php?did=" . DRAFT_ID . "\">Click here</a> to be taken to the draft's summary page, or <a href=\"public_draft.php?action=draftBoard&did=" . DRAFT_ID . "\">take a look at the draft board</a> to see all of the picks.");
@@ -105,6 +107,14 @@ switch(ACTION) {
 			define("PAGE_CONTENT", "An error has occurred and the draft was unable to be moved forward.");
 			require_once("views/shared/generic_result_view.php");
 			exit(1);
+		}
+		
+		if($DRAFT->isCompleted()) {
+			define("PAGE_HEADER", "Draft Room is Closed - Your Draft is Over!");
+			define("P_CLASS", "success");
+			define("PAGE_CONTENT", "The draft room is officially closed.  Your draft is officially complete, all picks have been made and now all you have to do is get the results entered into your league.<br /><br /><a href=\"public_draft.php?did=" . DRAFT_ID . "\">Click here</a> to be taken to the draft's summary page, or <a href=\"public_draft.php?action=draftBoard&did=" . DRAFT_ID . "\">take a look at the draft board</a> to see all of the picks.");
+			require_once("views/shared/generic_result_view.php");
+			exit(0);
 		}
 		
 		$NEXT_FIVE_PICKS = $DRAFT->getNextFivePicks();
