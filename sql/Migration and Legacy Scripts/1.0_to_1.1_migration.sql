6d11c5c09dfa03f735bee142f9a642a567341024
Matthew Orres <matthew.orres@gmail.com>
2012-01-26 17:46:24 -0500

    Update main SQL script and migration script.

--- sql/Migration and Legacy Scripts/1.0_to_1.1_migration.sql
+++ sql/Migration and Legacy Scripts/1.0_to_1.1_migration.sql
@@ -1,9 +1,18 @@
 -- PHPDraft Migration Script
 -- Only use if you have a pre-existing PHPDraft database (from version 1.0)
--- This migration script only handles the schema change associated with the addition of the
--- trades feature, which requires two new tables.
+-- *Schema change associated with the addition of the trades table
+-- *Schema change associated with the addition of the pro_players table (autocomplete)
+-- *Value updates to draft.draft_sport
+-- *Bug Fix - NBA position 'Shooting Guard' incorrectly abbreviated 'SH'
+--
+-- *NOTE: 1.1 adds autocomplete-assisted pick entry, in order to use this feature the
+-- pro_players table must be populated! Use the 'Update Pro Players' tool
+-- (control_panel.php?action=updateProPlayers) and the provided CSV files for each
+-- league (resources/ProPlayers_NFL.csv, etc.) to enable autocomplete.
 --
 -- No deprecated columns result from this migration.
+
+USE phpdraft;
 
 --
 -- Table structure for table `trades`
@@ -34,3 +43,46 @@ CREATE TABLE IF NOT EXISTS `trade_assets` (
 `was_drafted` TINYINT( 1 ) NOT NULL COMMENT 'Boolean whether the asset was a drafted player or not at the time of trade',
 INDEX ( `trade_id` , `player_id` , `oldmanager_id` , `newmanager_id` )
 ) ENGINE = MYISAM DEFAULT CHARSET=latin1 COMMENT = 'Table that tracks the assets that exchanged hands in a trade.' AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pro_players`
+--
+CREATE TABLE IF NOT EXISTS `pro_players` (
+  `pro_player_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of the player',
+  `league` text NOT NULL COMMENT 'Three character abbreviation of league player belongs to.',
+  `first_name` text NOT NULL COMMENT 'First name of the player',
+  `last_name` text NOT NULL COMMENT 'Last name of the player',
+  `position` text NOT NULL COMMENT 'Abbreviation of the position the player plays',
+  `team` text NOT NULL COMMENT 'Abbreviation of the city of the team the player plays for',
+  PRIMARY KEY (`pro_player_id`),
+  KEY `league_idx` (`league`(4)),
+  FULLTEXT KEY `firstname_idx` (`first_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Pro players used for auto-complete for pick entry' AUTO_INCREMENT=1 ;
+
+--
+-- Update draft_sport value to new values
+--
+UPDATE `draft`
+SET `draft_sport` = 'NFL'
+WHERE `draft_sport` = 'football';
+
+UPDATE `draft`
+SET `draft_sport` = 'NHL'
+WHERE `draft_sport` = 'hockey';
+
+UPDATE `draft`
+SET `draft_sport` = 'MLB'
+WHERE `draft_sport` = 'baseball';
+
+UPDATE `draft`
+SET `draft_sport` = 'NBA'
+WHERE `draft_sport` = 'basketball';
+
+--
+-- Update players with correct position
+--
+UPDATE `players`
+SET `position` = 'SG'
+WHERE `position` = 'SH';
