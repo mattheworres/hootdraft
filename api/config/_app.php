ea898e6edccac0eb95f8a8a557fb136dbac89fbe
Matthew Orres <matthew.orres@gmail.com>
2017-03-23 07:55:50 -0400

    Update the app settings pathing, too

--- api/config/_app.php
+++ api/config/_app.php
@@ -1,26 +1,26 @@
 <?php 
 
-require_once __DIR__.'/../../vendor/autoload.php';
+require_once dirname(__FILE__).'/../../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 //Handles settings users can define
 //Please see README.md for instructions on how to setup
-require_once __DIR__.'/../../appsettings.php';
+require_once dirname(__FILE__).'/../../appsettings.php';
 
 $app['debug'] = DEBUG_MODE;
 
-require_once __DIR__.'/_database.php';      //Sets up database connections
-require_once __DIR__.'/_log.php';           //Sets up logging
+require_once dirname(__FILE__).'/_database.php';      //Sets up database connections
+require_once dirname(__FILE__).'/_log.php';           //Sets up logging
 
 //Registrations with Pimple DI
-require_once __DIR__.'/_registerServices.php';
-require_once __DIR__.'/_registerRepositories.php';
-require_once __DIR__.'/_registerValidators.php';
-require_once __DIR__.'/_registerFactories.php';
+require_once dirname(__FILE__).'/_registerServices.php';
+require_once dirname(__FILE__).'/_registerRepositories.php';
+require_once dirname(__FILE__).'/_registerValidators.php';
+require_once dirname(__FILE__).'/_registerFactories.php';
 
-require_once __DIR__.'/_middlewares.php';   //Defines middleware handlers for shared logic, like ensuring a draft is editable
-require_once __DIR__.'/_router.php';        //Sets up controller routing
-require_once __DIR__.'/_security.php';      //Sets up Symfony-based security & user authentication
+require_once dirname(__FILE__).'/_middlewares.php';   //Defines middleware handlers for shared logic, like ensuring a draft is editable
+require_once dirname(__FILE__).'/_router.php';        //Sets up controller routing
+require_once dirname(__FILE__).'/_security.php';      //Sets up Symfony-based security & user authentication
 
 return $app;
