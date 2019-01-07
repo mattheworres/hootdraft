1f038a92d510f866b4110a0f781d1b0d5c3d92a3
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-01-07 00:36:06 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- api/config/_log.php
+++ api/config/_log.php
@@ -8,7 +8,7 @@ if (!$app instanceof Silex\Application) {
  * @global string LOGFILE_NAME
  */
 
-if($app['debug'] == true) {
+if ($app['debug'] == true) {
   $app->register(new Silex\Provider\MonologServiceProvider(), array(
       'monolog.logfile' => __DIR__ . '/logs/' . LOGFILE_NAME,
   ));
